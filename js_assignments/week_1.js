f220120f8c7a5d469e789c836e136bb7ab0d4af9
Mariam Mussa <mariamkilangi@outlook.com>
2025-09-07 18:29:46 +0300

    Week 1 - Javascript Assignment

---
+++ js_assignments/week_1.js
@@ -0,0 +1,20 @@
+let num1,num2,name;
+
+num1=5;
+num2=10;
+name="John";
+
+//addition
+console.log(num1+num2);
+
+//subtraction
+console.log(num2-num1);
+
+//multiplication
+console.log(num1*num2);
+
+//division
+console.log(num2/num1);
+
+const result = num1 + num2;
+console.log("Hello " + name + ", the result of num1 + num2 is " + result);
